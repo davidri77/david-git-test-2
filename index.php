7541ab5fe6254bb35bbf38b9eab7d619379251cd
davidri77 <davidrizky032@gmail.com>
2021-02-08 01:02:05 -0800

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo rem minus quibusdam earum iusto? Eaque optio incidunt molestiae enim ut, totam dignissimos! Aperiam consequatur quo magni id ipsum non fugiat.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
